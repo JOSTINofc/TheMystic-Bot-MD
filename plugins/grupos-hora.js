e92167252b3aecfe6b3873cd05affa138a88e626
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 11:15:34 -0500

    Actualizar grupos-hora.js

--- plugins/grupos-hora.js
+++ plugins/grupos-hora.js
@@ -1,12 +1,13 @@
 let media = './Menu.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-    await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
-let str = `*📍 GRUPOS OFICIALES*
+let str = `*⚡️ GRUPO OFICIAL*
+_${gp}_
 
-    *_╭━━━⊜ Grupos-Canales_*
-  *_┃🧸❏ ${grupos}_*
-*_╰━━━━━━━━━━━━━━━━⊜_*`
+*📍 CANALES OFICIALES*
+${channel}
+
+${channel2}`
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 
 handler.command = /^grupos|linksk|gruposofc|gruposoficiales$/i
