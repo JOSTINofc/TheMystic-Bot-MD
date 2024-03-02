a28e7789ade2fa7919331342954a46ac62255ac7
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 17:26:21 -0500

    Actualizar bot-menuprincipal.js

--- plugins/bot-menuprincipal.js
+++ plugins/bot-menuprincipal.js
@@ -1,400 +1,159 @@
-import fetch from 'node-fetch';
-const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+import { xpRange } from '../lib/levelling.js'
+//import { plugins } from '../lib/plugins.js'
+let tags = {
+  //Error
+}
+const defaultMenu = {
+  before: ` ╭─❒ *Usuario* 🌳
+│🗃 *Nombre:* %name
+│🔧 *Limite:* %diamond
+│📍 *Levelup:* %level
+│⚔️ *Rango:* %role
+│🧰 *Experincia:* %exp
+│
+├❒ *InfoBot* 🎈
+│🧩 *Creador:* Jostin
+│⏰️ *Tiempo Activo:* %muptime
+│👨‍💻 *Registrados:* %totalreg
+│
+├❒ *Fecha* 📅
+│⚡️ *Date:* %date
+╰──────────────
+`.trimStart(),
+  header: '╭•━━━ ✽ %category ✽ ━━━•',
+  body: '│🥀 %cmd',
+  footer: '╰•━━━ ✽ %level ✽ ━━━•\n',
+  after: `
+`,
+}
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+    let { exp, diamond, level, role } = global.db.data.users[m.sender]
+    let { min, xp, max } = xpRange(level, global.multiplier)
+    let name = await conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'es'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+      return {
+        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+        prefix: 'customPrefix' in plugin,
+        diamond: plugin.diamond,
+        premium: plugin.premium,
+        enabled: !plugin.disabled,
+      }
+    })
+    for (let plugin of help)
+      if (plugin && 'tags' in plugin)
+        for (let tag of plugin.tags)
+          if (!(tag in tags) && tag) tags[tag] = tag
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == conn.user.jid ? '' : `Powered by https://wa.me/${conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let _text = [
+      before,
+      ...Object.keys(tags).map(tag => {
+        return header.replace(/%category/g, tags[tag]) + '\n' + [
+          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+            return menu.help.map(help => {
+              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+                .replace(/%isdiamond/g, menu.diamond ? '(ⓓ)' : '')
+                .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '')
+                .trim()
+            }).join('\n')
+          }),
+          footer
+        ].join('\n')
+      }),
+      after
+    ].join('\n')
+    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    let replace = {
+      '%': '%',
+      p: _p, uptime, muptime,
+      me: conn.getName(conn.user.jid),
+      npmname: _package.name,
+      npmdesc: _package.description,
+      version: _package.version,
+      exp: exp - min,
+      maxexp: xp,
+      totalexp: exp,
+      xp4levelup: max - exp,
+      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
+      level, diamond, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+      readmore: readMore
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-  const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
-  const {money, joincount} = global.db.data.users[m.sender];
+    let pp = imagen4
+    // await conn.reply(m.chat, '*Enviando el menu . . .*', ftrol) 
+    conn.sendFile(m.chat, pp, 'menu2.jpg', text.trim(), m, null)
+    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
+      ['ꨄ︎ Apoyar', `${_p}donate`],
+      ['⏍ Info', `${_p}botinfo`],
+      ['⌬ Grupos', `${_p}gpdylux`]
+    ],m, rpl)*/
 
- const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
-    const rtotal = Object.entries(global.db.data.users).length || '0'
-        const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-  const {exp, limit, level, role} = global.db.data.users[m.sender];
-  const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/231b618729371a4e30f0e.jpg');
-  const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': saludo, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${saludo},;;;\nFN:${saludo},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}};
-    await conn.reply(m.chat, '*!𝗣𝗿𝗼𝘅𝗶𝗺𝗮𝗺𝗲𝗻𝘁𝗲 𝗦𝗲 𝗘𝗻𝘃𝗶𝗮𝗿𝗮́ 𝗘𝗹 𝗠𝗲𝗻𝘂́.*',m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '👋🏻 ¡𝐇𝐨𝐥𝐚!', body: '🦋 𝖫𝗂𝗇𝖽𝗈 𝖠𝗆𝖺𝗇𝖾𝖼𝖾𝗋 🧸', sourceUrl: global.md, thumbnail: await (await fetch(pp)).buffer() }}})
-//m.react('📍');
-    await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } })
-  let txt =`*╭✨️⸽⃕GOKUBOTLITE-MD🍁⃨፝⃕✰*
-┋ 🧸Autor: Jostim
-┋ 👤User: ${taguser}
-┋ 🌿Fecha: ${date}
-┋ 👥️️Usuario Registrados: ${rtotal}
-┋ 🦋Versión Del Bot: ${vs}
-╰━━━━━━━✦✗✦━━━━━━━━
 
-╭━━━━･❪ *INFO USER* ❫ ･━━━━╮
-┃✣➸ 📍Levelup: ${level}
-┃✣➸ 🧸Exp: ${exp}
-┃✣➸ ⚡️Rango: ${role}
-┃✣➸ 💎Diamantes: ${limit}
-┃✣➸ 💰GokuCoins: ${money}
-┃✣➸ 🪙Token: ${joincount}
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *SOLUCIONES* ❫ ･━━━━╮
-┃🥀 Mensajes en espera
-┃🥀 _!fixmsgespera_
-┃🥀 Mensajes en espera (owner)
-┃🥀 _!dsowner_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *INFO BOT* ❫ ･━━━━╮
-┃📍 _!instalarbot_
-┃📍 _!terminosycondiciones_
-┃📍 _!grupos_
-┃📍 _!estado_
-┃📍 _!infobot_
-┃📍 _!speedtest_
-┃📍 _!donar_
-┃📍 _!owner_
-┃📍 _!script_
-┃📍 _Bot_ (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━･❪ *BOT A TU GRUPO* ❫ ･━━╮
-┃🧸 _!join *<enlace / link / url>*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *SER SUBBOT* ❫ ･━━━━╮
-┃💻 _!botclone_
-┃💻 _!deletebot_
-┃💻 _!token_
-┃💻 _!stop_
-┃💻 _!bots_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━･❪ *HERRAMIENTAS* ❫ ･━━━╮
-┃🛠 _!enable restrict_
-┃🛠 _!disable restrict_
-┃🛠 _!enable autoread_
-┃🛠 _!disable autoread_
-┃🛠 _!enable antispam_
-┃🛠 _!disable antispam_
-┃🛠 _!enable anticall_
-┃🛠 _!disable anticall_
-┃🛠 _!enable modoia_
-┃🛠 _!disable modoia_
-┃🛠 _!enable audios_bot_
-┃🛠 _!disable audios_bot_
-┃🛠 _!enable antiprivado_
-┃🛠 _!disable antiprivado_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *DIVERSIONES* ❫ ･━━━━╮
-┃🔥 _!menujuegos_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━･❪ *ENABLE/DISABLE* ❫ ･━━━╮
-┃🌲 _!enable *welcome*_
-┃🌲 _!disable *welcome*_
-┃🌲 _!enable *modohorny*_
-┃🌲 _!disable *modohorny*_
-┃🌲 _!enable *antilink*_
-┃🌲 _!disable *antilink*_
-┃🌲 _!enable *antilink2*_
-┃🌲 _!disable *antilink2*_
-┃🌲 _!enable *juegos*_
-┃🌲 _!disable *juegos*_
-┃🌲 _!enable *detect*_
-┃🌲 _!disable *detect*_
-┃🌲 _!enable *audios*_
-┃🌲 _!disable *audios*_
-┃🌲 _!enable *autosticker*_
-┃🌲 _!disable *autosticker*_
-┃🌲 _!enable *antiviewonce*_
-┃🌲 _!disable *antiviewonce*_
-┃🌲 _!enable *antitoxic*_
-┃🌲 _!disable *antitoxic*_
-┃🌲 _!enable *antitraba*_
-┃🌲 _!disable *antitraba*_
-┃🌲 _!enable *antiarabes*_
-┃🌲 _!disable *antiarabes*_
-┃🌲 _!enable *modoadmin*_
-┃🌲 _!disable *modoadmin*_
-┃🌲 _!enable *antidelete*_
-┃🌲 _!disable *antidelete*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *REPORTE* ❫ ･━━━━╮
-┃🌹 _!reporte *<texto>*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *DESCARGAS* ❫ ･━━━━╮
-┃🛰 _!instagram *<enlace / link / url>*_
-┃🛰 _!mediafire *<enlace / link / url>*_
-┃🛰 _!gitclone *<enlace / link / url>*_
-┃🛰 _!gdrive *<enlace / link / url>*_
-┃🛰 _!tiktok *<enlace / link / url>*_
-┃🛰 _!tiktokimg *<enlace / link / url>*_
-┃🛰 _!xnxxdl *<enlace / link / url>*_
-┃🛰 _!xvideosdl *<enlace / link / url>*_
-┃🛰 _!twitter *<enlace / link / url>*_
-┃🛰 _!fb *<enlace / link / url>*_
-┃🛰 _!ytshort *<enlace / link / url>*_
-┃🛰 _!ytmp3 *<enlace / link / url>*_
-┃🛰 _!ytmp4 *<enlace / link / url>*_
-┃🛰 _!ytmp3doc *<enlace / link / url>*_
-┃🛰 _!ytmp4doc *<enlace / link / url>*_
-┃🛰 _!videodoc *<enlace / link / url>*_
-┃🛰 _!dapk2 *<enlace / link / url>*_
-┃🛰 _!stickerpack *<enlace / link / url>*_
-┃🛰 _!play *<texto>*_
-┃🛰 _!play2 *<texto>*_
-┃🛰 _!play.1 *<texto>*_
-┃🛰 _!play.2 *<texto>*_
-┃🛰 _!playdoc *<texto>*_
-┃🛰 _!playdoc2 *<texto>*_
-┃🛰 _!playlist *<texto>*_
-┃🛰 _!spotify *<texto>*_
-┃🛰 _!ringtone *<texto>*_
-┃🛰 _!soundcloud *<texto>*_
-┃🛰 _!imagen *<texto>*_
-┃🛰 _!pinterest *<texto>*_
-┃🛰 _!wallpaper *<texto>*_
-┃🛰 _!pptiktok *<nombre de usuario>*_
-┃🛰 _!igstalk *<nombre de usuario>*_
-┃🛰 _!igstory *<nombre de usuario>*_
-┃🛰 _!tiktokstalk *<username>*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━･❪ *BUSCADORES* ❫ ･━━━╮
-┃🔎 _!githubsearch *<texto>*_
-┃🔎 _!modapk *<texto>*_
-┃🔎 _!stickersearch *<texto>*_
-┃🔎 _!stickersearch2 *<texto>*_
-┃🔎 _!xnxxsearch *<texto>*_
-┃🔎 _!animeinfo *<texto>*_
-┃🔎 _!google *<texto>*_
-┃🔎 _!letra *<texto>*_
-┃🔎 _!wikipedia *<texto>*_
-┃🔎 _!ytsearch *<texto>*_
-┃🔎 _!playstore *<texto>*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━･❪ *GRUPO AJUSTES* ❫ ･━━╮
-┃⚙️ _!salir *<admins>*_
-┃⚙️ _!add *<numero>*_
-┃⚙️ _!kick *<@tag>*_
-┃⚙️ _!kick2 *<@tag>*_
-┃⚙️ _!mute *<@tag>*_
-┃⚙️ _!unmute *<@tag>*_
-┃⚙️ _!listanum *<texto>*_
-┃⚙️ _!kicknum *<texto>*_
-┃⚙️ _!grupo *<abrir / cerrar>*_
-┃⚙️ _!grouptime *<opcion> <tiempo>*_
-┃⚙️ _!promote *<@tag>*_
-┃⚙️ _!demote *<@tag>*_
-┃⚙️ _admins *<texto>*_ (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)
-┃⚙️ _!demote *<@tag>*_
-┃⚙️ _!infogroup_
-┃⚙️ _!resetlink_
-┃⚙️ _!link_
-┃⚙️ _!setname *<texto>*_
-┃⚙️ _!setdesc *<texto>*_
-┃⚙️ _!invocar *<texto>*_
-┃⚙️ _!setwelcome *<texto>*_
-┃⚙️ _!setbye *<texto>*_
-┃⚙️ _!hidetag *<texto>*_
-┃⚙️ _!hidetag *<audio>*_
-┃⚙️ _!hidetag *<video>*_
-┃⚙️ _!hidetag *<imagen>*_
-┃⚙️ _!warn *<@tag>*_
-┃⚙️ _!unwarn *<@tag>*_
-┃⚙️ _!listwarn_
-┃⚙️ _!fantasmas_
-┃⚙️ _!destraba_
-┃⚙️ _!setpp *<imagen>*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━･❪ *CONVERTIDORES* ❫ ･━━╮
-┃🦎 _!toanime *<imagen>*_
-┃🦎 _!togifaud *<video>*_
-┃🦎 _!toimg *<sticker>*_
-┃🦎 _!tomp3 *<video>*_
-┃🦎 _!tomp3 *<nota de voz>*_
-┃🦎 _!toptt *<video / audio>*_
-┃🦎 _!tovideo *<sticker>*_
-┃🦎 _!tourl *<video / imagen / audio>*_
-┃🦎 _!tts *<idioma> <texto>*_
-┃🦎 _!tts *<efecto> <texto>*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━･❪ *LOGOS Y EFECTOS* ❫ ･━━╮
-┃📌 _!logos *<efecto> <texto>*_
-┃📌 _!logochristmas *<texto>*_
-┃📌 _!logocorazon *<texto>*_
-┃📌 _!ytcomment *<texto>*_
-┃📌 _!hornycard *<@tag>*_
-┃📌 _!simpcard *<@tag>*_
-┃📌 _!lolice *<@tag>*_
-┃📌 _!itssostupid_
-┃📌 _!pixelar_
-┃📌 _!blur_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *PIROPOS* ❫ ･━━━━╮
-┃✏️  _!piropo_
-┃✏️  _!consejo_
-┃✏️  _!fraseromantica_
-┃✏️  _!historiaromantica_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━･❪ *MENU-ANIMES* ❫ ･━━━╮
-┃🍨 _!menuanimes_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *RANDOM* ❫ ･━━━━╮
-┃🛸 _!kpop *<blackpink / exo / bts>*_
-┃🛸 _!cristianoronaldo_
-┃🛸 _!messi_
-┃🛸 _!cat_
-┃🛸 _!dog_
-┃🛸 _!meme_
-┃🛸 _!itzy_
-┃🛸 _!blackpink_
-┃🛸 _!navidad_
-┃🛸 _!wpmontaña_
-┃🛸 _!pubg_
-┃🛸 _!wpgaming_
-┃🛸 _!wpaesthetic_
-┃🛸 _!wpaesthetic2_
-┃🛸 _!wprandom_
-┃🛸 _!wallhp_
-┃🛸 _!wpvehiculo_
-┃🛸 _!wpmoto_
-┃🛸 _!coffee_
-┃🛸 _!pentol_
-┃🛸 _!caricatura_
-┃🛸 _!ciberespacio_
-┃🛸 _!technology_
-┃🛸 _!doraemon_
-┃🛸 _!hacker_
-┃🛸 _!planeta_
-┃🛸 _!randomprofile_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━･❪ *COMANDO +18* ❫ ･━━━╮
-┃🔥 _!hornymenu_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━･❪ *EFECTOS-AUDIOS* ❫ ･━━╮
-┃𝘙𝘦𝘴𝘱𝘰𝘯𝘥𝘦 𝘈 𝘜𝘯𝘢 𝘕𝘰𝘵𝘢 𝘋𝘦 𝘈𝘶𝘥𝘪𝘰
-┃🔊 _!bass_
-┃🔊 _!blown_
-┃🔊 _!deep_
-┃🔊 _!earrape_
-┃🔊 _!fast_
-┃🔊 _!fat_
-┃🔊 _!nightcore_
-┃🔊 _!reverse_
-┃🔊 _!robot_
-┃🔊 _!slow_
-┃🔊 _!smooth_
-┃🔊 _!tupai_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━･❪ *CHAT ANONIMO* ❫ ･━━━╮
-┃🦊 _!start_
-┃🦊 _!next_
-┃🦊 _!leave_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━･❪ *MENU AUDIOS* ❫ ･━━━╮
-┃📒 _!menuaudios_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━･❪ *UTILIDADES* ❫ ･━━━╮
-┃🧩 _!inspect *<link wa_gc>*_
-┃🧩 _!chatgpt *<texto>*_
-┃🧩 _!delchatgpt_
-┃🧩 _!gptvoz *<texto>*_
-┃🧩 _!dall-e *<texto>*_
-┃🧩 _!spamwa *<numero|texto|cantidad>*_
-┃🧩 _!tamaño *<cantidad> <imagen / video>*_
-┃🧩 _!readviewonce *<imagen / video>*_
-┃🧩 _!clima *<país> <ciudad>*_
-┃🧩 _!encuesta *<texto1|texto2...>*_
-┃🧩 _!afk *<motivo>*_
-┃🧩 _!ocr *<responde a imagen>*_
-┃🧩 _!hd *<responde a imagen>*_
-┃🧩 _!acortar *<enlace / link / url>*_
-┃🧩 _!calc *<operacion math>*_
-┃🧩 _!del *<mensaje>*_
-┃🧩 _!whatmusic *<audio>*_
-┃🧩 _!readqr *<imagen (QR)>*_
-┃🧩 _!qrcode *<texto>*_
-┃🧩 _!readmore *<texto1| texto2>*_
-┃🧩 _!styletext *<texto>*_
-┃🧩 _!traducir *<texto>*_
-┃🧩 _!nowa *<numero>*_
-┃🧩 _!covid *<pais>*_
-┃🧩 _!horario_
-┃🧩 _!dropmail_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━･❪ *LIM/ECONOMIAS* ❫ ･━━╮
-┃⚡️ _!adventure_
-┃⚡️ _!cazar_
-┃⚡️ _!cofre_
-┃⚡️ _!balance_
-┃⚡️ _!claim_
-┃⚡️ _!heal_
-┃⚡️ _!lb_
-┃⚡️ _!levelup_
-┃⚡️ _!myns_
-┃⚡️ _!perfil_
-┃⚡️ _!work_
-┃⚡️ _!minar_
-┃⚡️ _!minar2_
-┃⚡️ _!buy_
-┃⚡️ _!buyall_
-┃⚡️ _!verificar_
-┃⚡️ _!robar *<cantidad> <@tag>*_
-┃⚡️ _!transfer *<tipo> <cantidad> <@tag>*_
-┃⚡️ _!unreg *<numero de serie>*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *STICKERS* ❫ ･━━━━╮
-┃🌳 _!sticker *<responder a imagen o video>*_
-┃🌳 _!sticker *<enlace / link / url>*_
-┃🌳 _!sticker2 *<responder a imagen o video>*_
-┃🌳 _!sticker2 *<enlace / link / url>*_
-┃🌳 _!s *<responder a imagen o video>*_
-┃🌳 _!s *<enlace / link / url>*_
-┃🌳 _!emojimix *<emoji 1>&<emoji 2>*_
-┃🌳 _!scircle *<imagen>*_
-┃🌳 _!sremovebg *<imagen>*_
-┃🌳 _!semoji *<tipo> <emoji>*_
-┃🌳 _!qc *<texto>*_
-┃🌳 _!attp *<texto>*_
-┃🌳 _!attp2 *<texto>*_
-┃🌳 _!attp3 *<texto>*_
-┃🌳 _!ttp *<texto>*_
-┃🌳 _!ttp2 *<texto>*_
-┃🌳 _!ttp3 *<texto>*_
-┃🌳 _!ttp4 *<texto>*_
-┃🌳 _!ttp5 *<texto>*_
-┃🌳 _!pat *<@tag>*_
-┃🌳 _!slap *<@tag>*_
-┃🌳 _!kiss *<@tag>*_
-┃🌳 _!dado_
-┃🌳 _!wm *<packname> <author>*_
-┃🌳 _!stickermarker *<efecto> <imagen>*_
-┃🌳 _!stickerfilter *<efecto> <imagen>*_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•
-
-╭━━━━･❪ *PROPIETARIO* ❫ ･━━━━╮
-┃💧 !menuowner
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•`;
-   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": '🧸⸽⃕  𝕾𝖆𝖐𝖚𝖗𝖆 - 𝕭𝖔𝖙 - 𝕷𝖎𝖙𝖊 - 𝕸𝕯 🦋⸽⃕✰', "containsAutoReply": true, "mediaType": 1, "thumbnail": [imagen6,imagen1,imagen4].getRandom(), "mediaUrl": global.gp1, "sourceUrl": global.gp1}}}, {quoted: fkon});
- // m.react('🧩');
-  } catch {
-    conn.reply(m.chat, '🧸 *Ocurrió Un Error*', m);
+  } catch (e) {
+    conn.reply(m.chat, '⚠️ Lo sentimos, el menú tiene un error', m)
+    throw e
   }
-};
-handler.help = ['menu'];
-handler.tags = ['menu'];
-handler.command = /^(menu|allmenu|menú|help|menucompleto)$/i;
+}
+handler.help = ['help']
+handler.tags = ['main']
+handler.command = ['menu'] 
 handler.register = true
+
+handler.exp = 3
+
 export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, 'd ', h, 'h ', m, 'm '].map(v => v.toString().padStart(2, 0)).join('')
+}
