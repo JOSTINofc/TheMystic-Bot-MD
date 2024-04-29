d081d7386bb2f307808d8b3a5ca74a72adbc98f2
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-04-29 15:24:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,113 +1,117 @@
-import {watchFile, unwatchFile} from 'fs';
-import chalk from 'chalk';
-import {fileURLToPath} from 'url';
-import fs from 'fs'; 
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
-import axios from 'axios';
-import moment from 'moment-timezone';
+importar  { watchFile ,  unwatchFile }  desde  'fs' ;
+importar  tiza  de  'tiza' ;
+importar  { fileURLToPath }  desde  'url' ;
+importar  fs  desde  'fs' ; 
+importar  cheerio  de  'cheerio' ;
+importar  buscar  desde  'node-fetch' ;
+importar  axios  desde  'axios' ;
+importar  momento  desde  'momento-zona horaria'
+importar  {  es  }  desde  "./lib/idiomas/total-idiomas.js"
 
-global.botnumber = ""
-global.confirmCode = ""
-
-global.owner = [
-  ['593939005387', '👑 Creador 👑', true],
-  ['5491130182250', '💖 Owner Ofc', true],
-  ['573013482814', '🧸 Desarrollador', true],
-  ['50558124470', '🍧 Ender', true],
-  ['573027866596', '🍭 Eder', true],
-  ['595992809980', 'IVAN 🔥', true],
-  ['595981477001'],
-  ['14437013473'],
-  ['51929972576'],
-  ['5491130182250']]
-
-global.goku = [
-  ["593939005387", '👑 Creador 👑', 1],
-  ["5491130182250", '💖 Owner', 1],
-  ["5492266466080", 'Agradecimiento 🌿', 1],
-  ["573013482814", '🧸 Desarrollador', 1],
-  ["573027866596", '✨️ Eder', 1]]
+global . propietario  =  [
+ [ '5491168758497' ,  '👑 𝘾𝙧𝙚𝙖𝙙𝙤𝙧 👑' ,  verdadero ] ,
+ [ '573012482597' ,  '𝙊𝙬𝙣𝙚𝙧 🧿' ,  verdadero ] ,
+ [ '593939005387' ,  '𝙊𝙬𝙣𝙚𝙧 2 🌼' ,  verdadero ] ,
+ [ '50558124470' ,  '𝗢𝘄𝗻𝗲𝗿 𝗧𝗮𝗹𝗹𝘆 🍧' ,  verdadero ] ]
 
 
-global.vs = 'V2'
+global . etiqueta de traje  =  [ '5491168758497' ] ;
+global . prems  =  [ '5491168758497' ] ;
 
-global.suittag = ['593939005387'];
-global.prems = ['593939005387'];
 
-global.packname = 'Sticker';
-global.author = '© Goku - Bot';
-global.wm = '© Goku - Bot';
-global.titulowm = '© Goku - Bot';
-global.titulowm2 = `© Goku - Bot`
-global.igfg = '© Goku - Bot';
-global.wait = '*[ ⏳ ] Cargando...*';
+global . nombre del paquete  =  '🍁 𝖣𝗂𝖺𝖻𝗅𝖺: 𝖡𝗈𝗍🪷' ;
+global . autor  =  '𝐃𝐢𝐚𝐛𝐥𝐚𝐁𝐨𝐭' ;
+global . wm  =  '🍁 𝕯𝖎𝖆𝖇𝖑𝖆𝕭𝖔𝖙-𝕸𝕯 🌸' ;
+global . sk  =  '🌹 𝐃𝐢𝐚𝐛𝐥𝐚𝐌𝐃 💮' ;
+global . titulowm  =  '🍁 𝖣𝗂𝖺𝖻𝗅𝖺 : 𝖡𝗈𝗍🪷' ;
+global . titulowm2  =  `𝖣𝗂𝖺𝖻𝗅𝖺 : 𝖡𝗈𝗍 - 𝖬𝖣 ☘️`
+global . igfg  =  '𝘋𝘪𝘢𝘣𝘭𝘪𝘵𝘢𝘉𝘰𝘵-𝘔𝘋' ;
+global . espera  =  '*⏳ 𝙲𝙰𝚁𝙶𝙰𝙽𝙳𝙾....*' ;
+global . frente  a  '1.2.0' ;
+global . vsJB  =  '1.0.7' ;
 
-global.imagen1 = fs.readFileSync('./Menu2.jpg');
-global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
-global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
-global.imagen4 = fs.readFileSync('./Menu.png');
-global.imagen5 = fs.readFileSync('./src/+18.jpg');
-global.imagen6 = fs.readFileSync('./Menu3.png');
+global . lenguajeSK  =  es  //idioma de diablabot
 
-global.mods = [];
+global . imagen1  =  fs . readFileSync ( './Menú2.jpg' ) ;
+global . imagen2  =  fs . readFileSync ( './src/nuevobot.jpg' ) ;
+global . imagen3  =  fs . readFileSync ( './src/NaufraMD.png' ) ;
+global . imagen4  =  fs . readFileSync ( './Menú.png' ) ;
+global . imagen5  =  fs . readFileSync ( './src/+18.jpg' ) ;
+global . imagen6  =  fs . readFileSync ( './Menu3.png' ) ;
+global . imagen7  =  fs . leerFileSync ( './Menu6.jpg' )
+global . imagen8  =  fs . leerFileSync ( './Menu4.jpg' )
 
-global.gp = 'https://chat.whatsapp.com/G9MpDVmJtbiIpItEZpg70s' //Grupo Oficial Del Bot
+//editor solo del bot.
+global . isdev  =  [ [ "5491168758497" ,  '👑 𝘾𝙧𝙚𝙖𝙙𝙤𝙧 👑' ,  1 ] ]
 
-global.channel = 'https://whatsapp.com/channel/0029VaRfhiH65yD3ch0g3X1n' //canal ofc
+global . gp1  =  'https://chat.whatsapp.com/DCjR7iTdg5q3Si4TbuwRjR'
+global . gp2  =  'https://www.paypal.me/ColaboracionBotOFC'
+global . gp3  =  'https://www.instagram.com/naufrazapp?igsh=Y2g1ZXJxbm53eXAy'
+global . gp4  =  'https://www.tiktok.com/@naufra.zapp?_t=8kcJigwvtji&_r=1'
+global . gp5  =  'https://chat.whatsapp.com/CH6Kc8UEpASLJCVasfZOuC'  //colaboración
+global . canal  =  'https://whatsapp.com/channel/0029VaFbERfFnSz5zeyfpi3L'
+global . paypal  =  ''
+global . yt  =  'https://youtube.com/@Enzito-19'
+global . md  =  'https://github.com/EnzoVaselevich/DiablaBot-MD-OFC-'
+global . fb  =  'https://www.facebook.com/share/8XyV52kz3sfhrzft/?mibextid=qi2Omg'
 
-global.channel2 = 'https://whatsapp.com/channel/0029VaHdt8oJENxtNExL8z2n' //canal desarrollador
+//MENSAJES ALEATORIOS:
+global . naufraMenus  =  [ imagen1 ,  imagen2 ,  imagen3 ,  imagen4 ,  imagen5 ,  imagen6 ,  imagen7 ,  imagen8 ]
+global . naufraRedes  =  [ gp1 ,  gp2 ,  gp3 ,  gp4 ,  canal ,  paypal ,  yt ,  md ,  fb ]
 
-//* *******Tiempo***************
-global.d = new Date(new Date + 3600000);
-global.locale = 'es';
-global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'});
-global.mes = d.toLocaleDateString('es', {month: 'long'});
-global.año = d.toLocaleDateString('es', {year: 'numeric'});
-global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
-//* ****************************
-global.wm2 = `${dia} ${fecha}\nGoku- Bot`;
-global.gt = 'Goku - Bot';
-global.mysticbot = 'Goku - Bot';
-global.md = 'https://github.com/JOSTINofc/TheMystic-Bot-MD';
-global.mysticbot = 'https://github.com/JOSTINofc/TheMystic-Bot-MD';
-global.waitt = '*[ ⏳ ] Cargando...*';
-global.waittt = '*[ ⏳ ] Cargando...*';
-global.waitttt = '*[ ⏳ ] Cargando...*';
-global.nomorown = '593939005387';
-global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
-global.cmenut = '❖––––––『';
-global.cmenub = '┊✦ ';
-global.cmenuf = '╰━═┅═━––––––๑\n';
-global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
-global.dmenut = '*❖─┅──┅〈*';
-global.dmenub = '*┊»*';
-global.dmenub2 = '*┊*';
-global.dmenuf = '*╰┅────────┅✦*';
-global.htjava = '⫹⫺';
-global.htki = '*⭑•̩̩͙⊱•••• ☪*';
-global.htka = '*☪ ••••̩̩͙⊰•⭑*';
-global.comienzo = '• • ◕◕════';
-global.fin = '════◕◕ • •';
-global.botdate = `*[ 📅 ] Fecha:*  ${moment.tz('America/Mexico_City').format('DD/MM/YY')}`;
-global.bottime = `*[ ⏳ ] Hora:* ${moment.tz('America/Mexico_City').format('HH:mm:ss')}`;
-global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
-global.multiplier = 99;
-global.flaaa = [
-  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
-  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
-  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
-];
-//* ************************
 
-const file = fileURLToPath(import.meta.url);
-watchFile(file, () => {
-  unwatchFile(file);
-  console.log(chalk.redBright('Update \'config.js\''));
-  import(`${file}?update=${Date.now()}`);
-});
+var  ase  =  nueva  fecha ( ) ;  var  hora  =  ase . obtenerHoras ( ) ;  switch ( hora ) {  caso  0 : hora  =  'Linda Mañana' ;  romper ;  caso  1 : hora  =  'Linda Mañana' ;  romper ;  caso  2 : hora  =  'Linda Mañana' ;  romper ;  caso  3 : hora  =  'Linda Mañana' ;  romper ;  caso  4 : hora  =  'linda mañana' ;  romper ;  caso  5 : hora  =  'Linda Mañana' ;  romper ;  caso  6 : hora  =  'Linda Mañana' ;  romper ;  caso  7 : hora  =  'Linda Mañana' ;  romper ;  caso  8 : hora  =  'Linda Mañana' ;  romper ;  caso  9 : hora  =  'Linda Mañana' ;  romper ;  caso  10 : hora  =  'Lindo Día' ;  romper ;  caso  11 : hora  =  'Lindo Día' ;  romper ;  caso  12 : hora  =  'Lindo Día' ;  romper ;  caso  13 : hora  =  'Lindo Día' ;  romper ;  caso  14 : hora  =  'Linda Tarde' ;  romper ;  caso  15 : hora  =  'Linda Tarde' ;  romper ;  caso  16 : hora  =  'Linda Tarde' ;  romper ;  caso  17 :hora  =  'Linda Tarde' ; romper ;  caso  18 : hora  =  'Linda Noche' ;  romper ;  caso  19 : hora  =  'Linda Noche' ;  romper ;  caso  20 : hora  =  'Linda Noche' ;  romper ;  caso  21 : hora  =  'Linda Noche' ;  romper ;  caso  22 : hora  =  'Linda Noche' ;  romper ;  caso  23 : hora  =  'Linda Noche' ;  romper ; }
+global . saludo  =  '🌹'  +  hora ;
 
-//Js
+global . modificaciones  =  [ ] ;
+
+global . d  =  nueva  fecha ( nueva  fecha  +  3600000 ) ;
+global . configuración regional  =  'es' ;
+global . diámetro  =  d . toLocaleDateString ( locale ,  { día laborable : 'largo' } ) ;
+global . fecha  =  d . toLocaleDateString ( 'es' ,  { día : 'numeric' ,  mes : 'numeric' ,  año : 'numeric' } ) ;
+global . mes  =  d . toLocaleDateString ( 'es' ,  { mes : 'largo' } ) ;
+global . año  =  d . toLocaleDateString ( 'es' ,  { año : 'numeric' } ) ;
+global . tiempo  =  d . toLocaleString ( 'en-US' ,  { hora : 'numérica' ,  minuto : 'numérica' ,  segundo : 'numérica' ,  hora12 : verdadero } ) ;
+
+
+global . wm2  =  ` ${ día }  ${ fecha } \nNaufraBot` ;
+global . gt  =  'DiablaBot' ;
+global . mysticbot  =  '𝗗𝗜𝗔𝗕𝗟𝗔𝗕𝗢𝗧-𝗠𝗗🌸' ;
+global . mysticbot  =  'https://whatsapp.com/channel/0029VaFbERfFnSz5zeyfpi3L' ;
+global . espera  =  '*⏳ 𝙲𝙰𝚁𝙶𝙰𝙽𝙳𝙾....*' ;
+global . waittt  =  '*⏳ 𝙲𝙰𝚁𝙶𝙰𝙽𝙳𝙾....*' ;
+global . waitttt  =  '*⏳ 𝙲𝙰𝚁𝙶𝙰𝙽𝙳𝙾....*' ;
+global . nomorown  =  '5491168758497' ;
+global . pdoc  =  [ 'application/vnd.openxmlformats-officedocument.presentationml.presentation' ,  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ,  'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ,  'application/vnd. ms-excel' ,  'aplicación/msword' ,  'aplicación/pdf' ,  'texto/rtf' ] ;
+global . cmenut  =  '❖––––––『' ;
+global . cmenub  =  '┊✦' ;
+global . cmenuf  =  '╰━═┅═━––––––๑\n' ;
+global . cmenua  =  '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n ' ;
+global . dmenut  =  '*❖─┅──┅〈*' ;
+global . dmenub  =  '*┊»*' ;
+global . dmenub2  =  '*┊*' ;
+global . dmenuf  =  '*╰┅────────┅✦*' ;
+global . htjava  =  '⫹⫺' ;
+global . htki  =  '*⭑•̩̩͙⊱•••• ☪*' ;
+global . htka  =  '*☪ ••••̩̩͙⊰•⭑*' ;
+global . comienzo  =  '• • ◕◕════' ;
+global . fin  =  '════◕◕ • •' ;
+global . botdate  =  `*📆 Fecha:*   ${ momento . tz ( 'América/Ciudad_de_México' ) . formato ( 'DD/MM/AA' ) } ` ;
+global . bottime  =  `*⏰ Hora:* ${ momento . tz ( 'América/Ciudad_de_México' ) . formato ( 'HH:mm:ss' ) } ` ;
+global . fgif  =  { clave : { participante : '0@s.whatsapp.net' } ,  mensaje : { 'videoMessage' : { 'título' : wm ,  'h' : `Hmm` ,  'segundos' : '999999999' ,  ' gifPlayback' : 'true' ,  'caption' : bottime ,  'jpegThumbnail' : fs . readFileSync ( './Menu.png' ) } } } ;
+global . multiplicador  =  99 ;
+global . flaaa  =  [
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=' ,
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=' ,
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' ,
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=' ,
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=¡Advertencia!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor 3Color=%23f2aa4c&fillColor4Color= %23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color= %23f2aa4c&backgroundColor=%23101820&text=' ,
+] ;
+
+
+const  archivo  =  fileURLToPath ( import.meta.url ) ;​​​​
+watchFile ( archivo ,  ( )  =>  {
+  unwatchFile ( archivo ) ;
+  consola . log ( tiza . redBright ( 'Actualizar \'config.js\'' ) ) ;
+  import ( ` ${ archivo } ? actualización = $ { Fecha.ahora ( ) } ` ) ;
+} ) ;
